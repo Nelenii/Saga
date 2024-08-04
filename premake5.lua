8387671e9d3e26117435859718f84ebb52411786
Nelenii <80850119+Nelenii@users.noreply.github.com>
2024-08-04 23:25:12 +0200

    Project: add premake setup to generate projects

---
+++ premake5.lua
@@ -0,0 +1,134 @@
+workspace "Saga"
+
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Distribution"
+	}
+
+
+
+outputDirectory = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Saga"
+
+	location "Saga"
+	kind "SharedLib"
+	language "C++"
+	targetdir ("binaries/" .. outputDirectory .. "/%{prj.name}")
+	objdir ("intermediate/" .. outputDirectory .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/source/**.h",
+		"%{prj.name}/source/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/thirdparty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SAGA_PLATFORM_WINDOWS",
+			"SAGA_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../binaries/" .. outputDirectory .. "/GameTemplate")
+		}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+		defines 
+		{
+			"SAGA_DEBUG"
+		}
+
+	filter "configurations:Release"
+		optimize "On"
+
+		defines 
+		{
+			"SAGA_RELEASE"
+		}
+
+	filter "configurations:Distribution"
+		optimize "On"
+
+		defines 
+		{
+			"SAGA_DISTRIBUTION"
+		}
+
+
+
+project "GameTemplate"
+
+	location "GameTemplate"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("binaries/" .. outputDirectory .. "/%{prj.name}")
+	objdir ("intermediate/" .. outputDirectory .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/source/**.h",
+		"%{prj.name}/source/**.cpp"
+	}
+
+	includedirs
+	{
+		"Saga/thirdparty/spdlog/include",
+		"Saga/source"
+	}
+
+	links
+	{
+		"Saga"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"SAGA_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		symbols "On"
+
+		defines 
+		{
+			"SAGA_DEBUG"
+		}
+
+	filter "configurations:Release"
+		optimize "On"
+
+		defines 
+		{
+			"SAGA_RELEASE"
+		}
+
+	filter "configurations:Distribution"
+		optimize "On"
+
+		defines 
+		{
+			"SAGA_DISTRIBUTION"
+		}
